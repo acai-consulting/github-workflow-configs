4fd612046697a17f822d69ec15e3094855ea569a
Michael Ullrich <83631137+michael-ullrich-1010@users.noreply.github.com>
2024-04-11 09:22:46 +0200

    fix: Support for nested main.tf (#3)
    ---------
    
    Co-authored-by: Michael Ullrich <michael.ullrich@nuvibit.com>

--- semantic-release/release.config.js
+++ semantic-release/release.config.js
@@ -12,42 +12,46 @@ module.exports = {
         [
             "@semantic-release/exec",
             {
-                prepareCmd: "sed -i 's|\\(/\\*inject_version_start\\*/ \"\\).*\\(\" /\\*inject_version_end\\*/\\)|\\1${nextRelease.version}\\2|' main.tf"
+                // This command updates all main.tf files with the new version
+                prepareCmd: "find . -type f -name 'main.tf' -exec sed -i 's|\\(/\\*inject_version_start\\*/ \"\\).*\\(\" /\\*inject_version_end\\*/\\)|\\1${nextRelease.version}\\2|' {} +"
             }
         ],
         [
             "@semantic-release/exec",
             {
+                // This command updates the README.md with a simple version placeholder replacement
                 prepareCmd: "sed -i 's|INJECT_VERSION|${nextRelease.version}|g' README.md"
             }
         ],
         [
             "@semantic-release/exec",
             {
-                prepareCmd: "sed -i 's|module_version-[0-9]*\.[0-9]*\.[0-9]*|module_version-${nextRelease.version}|g' README.md"
-            }            
+                // This command updates the README.md with a more complex version placeholder replacement
+                prepareCmd: "sed -i 's|module_version-[0-9]*\\.[0-9]*\\.[0-9]*|module_version-${nextRelease.version}|g' README.md"
+            }
         ],
         [
             '@semantic-release/github',
             {
                 successComment: "This ${issue.pull_request ? 'PR is included' : 'issue has been resolved'} in version ${nextRelease.version} :tada:",
                 labels: false,
-                releasedLabels: false
+                releasedLabels: false,
             }
         ],
         [
             "@semantic-release/changelog",
             {
                 changelogFile: './CHANGELOG.md',
-                changelogTitle: '# Changelog\n\nAll notable changes to this project will be documented in this file.'
+                changelogTitle: '# Changelog\n\nAll notable changes to this project will be documented in this file.',
             }
         ],
         [
             '@semantic-release/git',
             {
-                assets: ['CHANGELOG.md', 'main.tf', 'README.md'], // Include main.tf to commit the updated version
-                message: 'chore(release): version ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
+                // Exclude main.tf from this step's assets, since their inclusion and commit are handled by the exec plugin.
+                assets: ['CHANGELOG.md', 'README.md', '**/main.tf'], // Only include files directly handled here
+                message: 'chore(release): version ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
             }
-        ],
+        ]
     ]
 };
